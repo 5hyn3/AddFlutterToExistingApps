8e3fcfb466fe679574a064b3e1234e65ef5fef1f
Shoya Yamada <shoya-yamada@retty.me>
2018-08-08 12:30:20 +0900

    add flutter activity

--- app/src/main/java/com/example/addfluttertoexistingapps/MainActivity.kt
+++ app/src/main/java/com/example/addfluttertoexistingapps/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.addfluttertoexistingapps
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import io.flutter.facade.Flutter
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val flutterView = Flutter.createView(
+                this@MainActivity,
+                lifecycle,
+                "route1"
+        )
+        val layout = FrameLayout.LayoutParams(600, 800)
+        layout.leftMargin = 100
+        layout.topMargin = 200
+        addContentView(flutterView, layout)
     }
 }
